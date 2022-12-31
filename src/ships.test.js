26319209bff57e27864bb12933482fd5429b5ec2
Somtochukwu Francis <somtofrancis5@gmail.com>
2022-12-31 23:40:20 +0100

    Test Ship's Public Interface

---
+++ src/ships.test.js
@@ -0,0 +1,35 @@
+import {playerCarrier, playerDestroyer} from './ships';
+
+test('Destroyer length is 3', ()=>{
+    expect(playerDestroyer.length).toBe(3);
+});
+
+test('Destroyer Floats', ()=>{
+    expect(playerDestroyer.isSunk()).toBeFalsy();
+});
+
+describe('Test the hit method',()=>{
+    beforeAll(()=>{
+        return playerCarrier.hit(), playerCarrier.hit(), playerCarrier.hit(), playerCarrier.hit(), playerCarrier.hit();
+    });
+    // beforeAll(()=>{
+    //     return playerCarrier.hit();
+    // });
+    // beforeAll(()=>{
+    //     return playerCarrier.hit();
+    // });
+    // beforeAll(()=>{
+    //     return playerCarrier.hit();
+    // });
+    // beforeAll(()=>{
+    //     return playerCarrier.hit();
+    // });
+
+    test("Ship's damage count increases on hit", ()=>{
+        expect(playerCarrier.damageCount).toEqual(5);
+    });
+
+    test("Carrier should sink on five hits", ()=>{
+        expect(playerCarrier.isSunk()).toBeTruthy();
+    });
+})
