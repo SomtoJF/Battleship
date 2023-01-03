8fa4140e636abbf17f47b3146c9cd8011e359ac9
Somtochukwu Francis <somtofrancis5@gmail.com>
2023-01-03 02:06:15 +0100

    Write tests to test vertical placement of ships

--- src/Tests/gameboard.test.js
+++ src/Tests/gameboard.test.js
@@ -1,29 +1,27 @@
-import gameBoard from './gameBoard';
-import {carrier, destroyer} from './ships';
+import gameBoard from '../Objects/gameBoard';
+import {carrier, destroyer} from '../Objects/ships';
 
 const testBoard = new gameBoard();
 
 describe('Test performance on placement of ships', ()=>{
     beforeAll(()=>{
-        return testBoard.place(0,0, new carrier());
+        return testBoard.place(0,0, new carrier()), 
+        testBoard.place(9,0,new destroyer()),
+        testBoard.place(5, 2, new destroyer(), false)
     });
 
-    beforeAll(()=>{
-        return testBoard.place(9,0, new destroyer());
-    });
-
-    test('Ships can be placed on the board', ()=>{
+    test('Ships can be placed Horizontally', ()=>{
         expect(testBoard.board[0][0]).toEqual(expect.objectContaining({
-            length: 5
-        }));
-    });
-
-    test('Ships length are taken into account during placement', ()=>{
-        expect(testBoard.board[0][4]).toEqual(expect.objectContaining({
             length: 5
         }));
 
         expect(testBoard.board[9][2]).toEqual(expect.objectContaining({
+            length: 3
+        }));
+    });
+
+    test('Ships can be placed vertically', ()=>{
+        expect(testBoard.board[7][2]).toEqual(expect.objectContaining({
             length: 3
         }));
     });
@@ -34,7 +32,15 @@ describe('Test performance on placement of ships', ()=>{
 
         expect(()=>{testBoard.place(9,8,new carrier())}).toThrow('Ship will overflow the grid');
 
+        expect(()=>{testBoard.place(9,0,new carrier(), false)}).toThrow('Space is occupied by another ship');
+
+        expect(()=>{testBoard.place(9,9,new carrier(), false)}).toThrow('Ship will overflow the grid');
+
         expect(testBoard.board[9][2]).not.toEqual(expect.objectContaining({
+            length: 5
+        }));
+
+        expect(testBoard.board[7][9]).not.toEqual(expect.objectContaining({
             length: 5
         }));
     });
