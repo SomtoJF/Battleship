2e7424750497c3cad2de8453d8a4ab6d196b9af6
Somtochukwu Francis <somtofrancis5@gmail.com>
2023-01-03 02:07:44 +0100

    Modify placement method to place ships vertically

--- src/Objects/gameBoard.js
+++ src/Objects/gameBoard.js
@@ -15,7 +15,7 @@ class gameBoard
         return arr;
     };
 
-    #canBePlaced(x,y,length)
+    #canBePlaced(x, y, length, isHorizontal)
     {
         const myBoard = this.board;
         isUnoccupied();
@@ -24,39 +24,68 @@ class gameBoard
 
         function isUnoccupied()
         {
-            for(let i = 0; i < length; i++){
-                if(myBoard[x][y + i] != undefined)
-                {
-                    throw new Error('Space is occupied by another ship')
+            if(isHorizontal == true)
+            {
+                for(let i = 0; i < length; i++){
+                    if(myBoard[x][y + i] != undefined)
+                    {
+                        throw new Error('Space is occupied by another ship')
+                    };
+                };
+            }
+            else{
+                for(let i = 0; i < length; i++){
+                    if(myBoard[x + i][y] != undefined)
+                    {
+                        throw new Error('Space is occupied by another ship')
+                    };
                 };
             };
+            
         };
 
         function isLocationValid(){
-            if((y + length) > 9)
+            if(isHorizontal == true && ((y + length) > 9))
             {
                 throw new Error('Ship will overflow the grid')
-            }
-            else if(x > 9 || y > 9)
+            };
+            
+            if(isHorizontal == false && ((x + length) > 9))
+            {   
+                throw new Error('Ship will overflow the grid')
+            };
+
+            if(x > 9 || y > 9)
             {
                 throw new Error('location does not exist')
             };
+            
         };
     };
 
-    place(x,y,ship)
+    place(x, y, ship, isHorizontal = true)
     {
         const length = ship.length;
         try{
-            this.#canBePlaced(x, y, length);
-            let placementCounter = 0;
+            this.#canBePlaced(x, y, length, isHorizontal);
             // let ship occupy all array elements from the start of y which span it's length
-            while(placementCounter < length)
+            let placementCounter = 0;
+            if(isHorizontal == true)
             {
-                this.board[x][y] = ship;
-                y++;
-                placementCounter++;
+                while(placementCounter < length)
+                {
+                    this.board[x][y + placementCounter] = ship;
+                    placementCounter++;
+                };
+            }
+            else{
+                while(placementCounter < length)
+                {
+                    this.board[x + placementCounter][y] = ship;
+                    placementCounter++;
+                };
             };
+            
         }
         catch(e){
             throw new Error(e);
@@ -67,5 +96,6 @@ class gameBoard
 // let testBoard = new gameBoard();
 // testBoard.place(0,0,{length:5});
 // testBoard.place(0,7,{length:3});
-// console.log(testBoard.board[0][1]);
+// testBoard.place(6,7,{length:5}, false)
+// console.log(testBoard.board);
 export default gameBoard;
