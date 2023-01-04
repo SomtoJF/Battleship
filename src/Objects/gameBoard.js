7d3689ce22d2e91760cd7e407559f50178a92be8
Somtochukwu Francis <somtofrancis5@gmail.com>
2023-01-04 15:16:00 +0100

    Write and test receive attack function

--- src/Objects/gameBoard.js
+++ src/Objects/gameBoard.js
@@ -4,6 +4,56 @@ class gameBoard
 {
     constructor(){
         this.board = this.#emptyBoard();
+        this.shots = [];
+        this.clear = ()=>{
+            this.board = this.#emptyBoard();
+            this.shots = [];
+        };
+    };
+
+    place(x, y, ship, isHorizontal = true)
+    {
+        const length = ship.length;
+        try{
+            this.#canBePlaced(x, y, length, isHorizontal);
+            // let ship occupy all array elements from the start of y which span it's length
+            let placementCounter = 0;
+            if(isHorizontal == true)
+            {
+                while(placementCounter < length)
+                {
+                    this.board[x][y + placementCounter] = ship;
+                    placementCounter++;
+                };
+            }
+            else{
+                while(placementCounter < length)
+                {
+                    this.board[x + placementCounter][y] = ship;
+                    placementCounter++;
+                };
+            };
+            
+        }
+        catch(e){
+            throw new Error(e);
+        };
+    };
+
+    receiveAttack(x, y)
+    {
+        const myBoard = this.board;
+        this.#isShotBefore(x, y);
+        if(myBoard[x][y] != undefined)
+        {
+            myBoard[x][y].hit();
+            this.#recordHit(x, y);
+            return 'Hit';
+        }
+        else{
+            this.#recordMiss(x, y);
+            return 'Miss';
+        };
     };
 
     #emptyBoard()
@@ -70,32 +120,34 @@ class gameBoard
         };
     };
 
-    place(x, y, ship, isHorizontal = true)
+    #isShotBefore(x, y)
     {
-        const length = ship.length;
-        try{
-            this.#canBePlaced(x, y, length, isHorizontal);
-            // let ship occupy all array elements from the start of y which span it's length
-            let placementCounter = 0;
-            if(isHorizontal == true)
+        const myShots = this.shots;
+        for(let i = 0; i < myShots.length; i++){
+            if(myShots[i].x == x && myShots[i].y == y)
             {
-                while(placementCounter < length)
-                {
-                    this.board[x][y + placementCounter] = ship;
-                    placementCounter++;
-                };
-            }
-            else{
-                while(placementCounter < length)
-                {
-                    this.board[x + placementCounter][y] = ship;
-                    placementCounter++;
-                };
+                throw new Error('Location Shot before');
             };
-            
-        }
-        catch(e){
-            throw new Error(e);
+        };
+    };
+
+    #recordHit(x,y)
+    {
+        const myShots = this.shots;
+        myShots[myShots.length] = {
+            x: x,
+            y: y,
+            isHit: true
+        };
+    };
+
+    #recordMiss(x,y)
+    {
+        const myShots = this.shots;
+        myShots[myShots.length] = {
+            x: x,
+            y: y,
+            isHit: false
         };
     };
 };
--- src/Tests/gameboard.test.js
+++ src/Tests/gameboard.test.js
@@ -1,6 +1,30 @@
 import gameBoard from '../Objects/gameBoard';
 import {carrier, destroyer} from '../Objects/ships';
 
+// use toContainObject() matcher to test if an array contains a particular object
+expect.extend({
+    toContainObject(received, argument) {
+  
+      const pass = this.equals(received, 
+        expect.arrayContaining([
+          expect.objectContaining(argument)
+        ])
+      )
+  
+      if (pass) {
+        return {
+          message: () => (`expected ${this.utils.printReceived(received)} not to contain object ${this.utils.printExpected(argument)}`),
+          pass: true
+        }
+      } else {
+        return {
+          message: () => (`expected ${this.utils.printReceived(received)} to contain object ${this.utils.printExpected(argument)}`),
+          pass: false
+        }
+      }
+    }
+  })
+
 const testBoard = new gameBoard();
 
 describe('Test performance on placement of ships', ()=>{
@@ -8,6 +32,10 @@ describe('Test performance on placement of ships', ()=>{
         return testBoard.place(0,0, new carrier()), 
         testBoard.place(9,0,new destroyer()),
         testBoard.place(5, 2, new destroyer(), false)
+    });
+
+    afterAll(()=>{
+        return testBoard.clear();
     });
 
     test('Ships can be placed Horizontally', ()=>{
@@ -26,7 +54,6 @@ describe('Test performance on placement of ships', ()=>{
         }));
     });
 
-    // This is not passing
     test('Cannot place a ship on an occupied area', ()=>{
         expect(()=>{testBoard.place(9,2,new carrier())}).toThrow('Space is occupied by another ship');
 
@@ -45,3 +72,39 @@ describe('Test performance on placement of ships', ()=>{
         }));
     });
 });
+
+describe('Test recieve Attack functions', ()=>{
+    beforeEach(()=>{
+        return testBoard.place(0, 0, new carrier()),
+        testBoard.receiveAttack(0, 1)
+    });
+
+    afterEach(()=>{
+        return testBoard.clear();
+    });
+
+    test('Gameboard receives attack and returns if it as a hit or miss', ()=>{
+        expect(testBoard.receiveAttack(0, 3)).toBe('Hit');
+        expect(testBoard.receiveAttack(5, 5)).toBe('Miss');
+    });
+
+    test('Receiving attacks increments damage Count', ()=>{
+        testBoard.receiveAttack(0, 2);
+        expect(testBoard.board[0][0].damageCount).toEqual(2);
+    });
+
+    test('Shots are being recorded', ()=>{
+        expect(testBoard.shots).toContainObject({
+            x: 0,
+            y: 1,
+            isHit: true
+        });
+
+        testBoard.receiveAttack(5, 5)
+        expect(testBoard.shots).toContainObject({
+            x: 5,
+            y: 5,
+            isHit: false
+        });
+    });
+})
